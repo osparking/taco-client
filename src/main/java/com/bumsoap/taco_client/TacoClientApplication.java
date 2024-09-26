49b59f8da460b70a2bd96d0c6dd4092ae13eb8fb
osparking <jbpark03@gmail.com>
2024-09-26 19:50:07 +0900

    initial taco rest service client

---
+++ src/main/java/com/bumsoap/taco_client/TacoClientApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.taco_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumsoap/taco_client/TacoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumsoap.taco_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
